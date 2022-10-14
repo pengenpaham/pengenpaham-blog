ec858116a57a9da7a787dca99e81947a2b2adb07
ervinismu <social.ervin@gmail.com>
2022-10-14 22:19:38 +0700

    chore : organize folder add setup logo

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,30 @@
 export default {
     title: "PengenPaham",
-    description: "Blog for learning"
+    description: "Blog for learning",
+    lastUpdated: true,
+    themeConfig: {
+        logo: 'logo.png',
+        nav: [
+            { text: 'Contribution Guide', link: '/contribution-guide' },
+            { text: 'Learning Path', link: '/learning-path' },
+            { text: 'Roadmap', link: '/roadmap' },
+        ],
+        socialLinks: [
+            { icon: 'instagram', link: 'https://instagram.com/pengenpaham' },
+        ],
+        sidebar: [
+            {
+              text: 'Guide',
+              items: [
+                { text: 'Contribution Guide', link: '/contribution-guide' },
+                { text: 'Learning Path', link: '/learning-path' },
+                { text: 'Roadmap', link: '/roadmap' },
+              ]
+            }
+        ],
+        footer: {
+            message: 'Released under the MIT License.',
+            copyright: 'Copyright © 2022-present @pengenpaham'
+        }
+    }
 }
